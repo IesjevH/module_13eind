c79b89624f7b9ffb88983e2ea8eef7b0f0833f9e
Isis <isisvanhemert@gmail.com>
2023-11-09 09:27:14 +0100

    stap 1, 2, 3

--- html-template/detail.php
+++ html-template/detail.php
@@ -1,35 +1,60 @@
+<?php include("connection.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Portfolio Website - Overzichtspagina</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
-    <main>
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1 projects">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Portfolio Website - Overzichtspagina</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+
+
+  <main>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 g-1 projects">
+
+        <?php
+    
+       $stmt = $conn->prepare("SELECT * FROM projects WHERE id = :id");
+        $stmt->bindParam(':id', $_GET["id"]);
+
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach ($stmt->fetchAll() as $k => $v) { ?>
+
           <div id="project1" class="project card shadow-sm card-body m-2">
             <div class="card-text">
-              <h2>Titel van project 1.</h2>
-              <div>Hier komt een <b>lange</b> omschrijving van het project.</div>
-              <div>Type: website</div>
-              <div>Jaar: 2023</div>
+              <a href="detail.php?id=<?php echo $v["id"]; ?>">
+                <h2>Project
+                  <?php echo $v["titel"]; ?>
+                </h2>
+                <div>
+                  <?php echo $v["lange_omschrijving"]; ?>
+                </div>
+                <div>Type:
+                  <?php echo $v["type"]; ?>
+                </div>
+                <div>Jaar:
+                  <?php echo $v["jaar"] ?>
+                </div>
             </div>
           </div>
-        </div>
+
+        <?php } ?>
+
+
+
       </div>
-    </main>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+    </div>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
